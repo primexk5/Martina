7f6552f70fb153295b613e8fc354522f5359ce9d
pero4l <lilpero03@gmail.com>
2025-10-28 20:38:31 +0100

    Building the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+// import { CiMenuFries } from "react-icons/ci";
 
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        <h1>My Application</h1>
+      <nav className='lg:flex-row lg:flex items-center justify-between'>
+
+      <div className='flex items-center justify-between p-3'>
+          <div>
+           <h1 className='text-4xl font-semibold text-blue-800'>Martina</h1>
+           <p className='text-sm'>Centre for Sustainable</p>
+        </div>
+        {/* <CiMenuFries/> */}
+        <p className='lg:hidden'>M</p>
+      </div>
+
+        <div>
+          <ul className=' text-center lg:text-start flex flex-col lg:flex-row gap-7'>
+            <li className='cursor-pointer'>Home</li>
+            <li className='cursor-pointer'>About</li>
+            <li className='cursor-pointer'>Services</li>
+            <li className='cursor-pointer'>Contact</li>
+          </ul>
+        </div>
+
+        <div></div>
+
       </nav>
     </div>
   )
