55eb7a37e0c6c7f071fb711b1d74da70133448c7
Barnabas Lapshak <lapshakbarnabas4@gmail.com>
2025-10-28 17:57:43 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './molecules/About'
+import Objectives from './molecules/Objectives'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Objectives />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-screen bg-blue-200'>
+        <h1>Hero page</h1>
+    </div>
+  )
+}
+
+export default Hero
